01a7b3efdb9c6836bb1c66e89edee1820a72fb0a
Magak Emmanuel <emashmagak@gmail.com>
2020-10-13 20:10:01 +0300

    added login tests

--- tests/Unit/AuthControllerTest.php
+++ tests/Unit/AuthControllerTest.php
@@ -4,7 +4,9 @@
 namespace Tests\Unit;
 
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase;
+use Laravel\Sanctum\Sanctum;
 use Tests\CreatesApplication;
 
 class AuthControllerTest extends TestCase
@@ -62,4 +64,59 @@ class AuthControllerTest extends TestCase
 
         $this->assertJson($expected, json_encode($result));
     }
+
+    public function testUserLogin()
+    {
+//        $user = Sanctum::actingAs(
+//            factory(User::class)->create(),
+//            ['*']
+//        );
+        $user = User::factory()->create();
+        $request = [
+            'email' => $user->email,
+            'password' => 'password',
+        ];
+        $result = $this->postJson('api/v1/login', $request)->json();
+
+        $expected = [
+            'message' => 'Login successfull',
+            'token' => '3|BB54L8Fk0IXVpkvvYExERUGGK5C6eguSnD92dHMa',
+            'user' =>
+                [
+                    'id' => 3,
+                    'name' => 'ALX APP',
+                    'email' => 'user@mail.com',
+                    'email_verified_at' => '2020-10-12T18:06:53.000000Z',
+                    'created_at' => '2020-10-12T18:06:53.000000Z',
+                    'updated_at' => '2020-10-12T18:06:53.000000Z',
+                ],
+        ];
+
+
+        $this->assertEquals($result['message'], $expected['message']);
+    }
+
+    public function testUserLoginValidationError()
+    {
+        $user = User::factory()->create();
+        $request = [
+            'email' => $user->email,
+            'password' => 'secret',
+        ];
+
+        $result = $this->postJson('api/v1/login', $request)->json();
+
+        $expected = '{
+            "message": "The given data was invalid.",
+            "errors": {
+                "email": [
+                    "The provided credentials are incorrect."
+                ]
+            }
+        }';
+
+
+        $this->assertJson($expected, json_encode($result));
+    }
+
 }
